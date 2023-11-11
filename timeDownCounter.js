6231f7d7d3fd8e2b2195caf291e73f5b1605ed17
Manan Kher <125725838+MacaroniMutton@users.noreply.github.com>
2023-11-11 20:41:47 +0530

    Add files via upload

---
+++ timeDownCounter.js
@@ -0,0 +1,28 @@
+var end = new Date('11/14/2023 12:00 AM');
+
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+
+    function showRemaining() {
+        var now = new Date();
+        var distance = end - now;
+        if (distance < 0) {
+
+            clearInterval(timer);
+            var divContain = document.querySelector('div.container');
+            divContain.style.justifyContent = 'center';
+            divContain.innerHTML = '<div id="temp"><a href="./public/FlyingRocket.html" class="button"><svg><rect x="0" y="0" fill="none" width="100%" height="100%" /></svg>Click to Launch</a></div>';
+            return;
+        }
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+
+        document.getElementById('countdown').innerHTML = days + ' : ' + hours + " : " + minutes + " : " + seconds;
+    }
+
+    timer = setInterval(showRemaining, 1000);
